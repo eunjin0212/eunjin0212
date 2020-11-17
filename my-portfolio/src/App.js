fa6a61c711811b7cd4bf4f963a8591839af0a8d3
eunjin0212 <eunjin9639@gmail.com>
2020-11-17 22:24:10 +0900

    portfolio

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import Loading from "./Components/Loading";
-import About from './Components/About'
+import About from "./Components/About";
 import Header from "./Components/Header";
-import Cover from "./Components/Cover"
 
-import "../src/CSS/index.css"
-import "../src/CSS/about.css"
+import "../src/CSS/index.css";
+import "../src/CSS/about.css";
 class App extends Component {
   render() {
     return (
       <>
-      <Header/>
+        <Header />
         <Loading />
-        <Cover/>
         <About />
       </>
     );
--- my-portfolio/src/Components/Cover.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Cover = () => {
-  //Hooks
-  //2020.11.17
-};
-
-export default Cover;
