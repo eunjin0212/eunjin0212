573ea30658fa0f866f63afd72d0394d4e68c2cd5
eunjin0212 <eunjjin9639@gmail.com>
2021-07-11 23:26:56 +0900

    이메일 보내기 추가, css 변경

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 const Header = () => {
-    return (
-        <HeaderWrapper>
-            <Title>Genie's Web Portfolio</Title>
-            <Email>eunjin9639@gamil.com</Email>
-        </HeaderWrapper>
-    );
-};
+  return (
+    <HeaderWrapper>
+      <Title>Genie's Web Portfolio</Title>
+      <Email href="mailto:someone@yoursite.com" target="_blank">
+        eunjin9639@gamil.com
+      </Email>
+    </HeaderWrapper>
+  )
+}
 
-export default Header;
+export default Header
 const HeaderWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    background-color: inherit;
-    color: white;
-    margin: 10px;
-`;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  background-color: inherit;
+  color: #00cdac;
+  padding: 10px;
+`
 
 const Title = styled.div`
-    font-weight: bold;
-`;
+  font-weight: bold;
+`
 
-const Email = styled.div`
-    color: #bdc3c7;
-    font-weight: lighter;
-`;
+const Email = styled.a`
+  color: #bdc3c7;
+  font-weight: lighter;
+`
